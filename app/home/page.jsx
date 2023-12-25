ff7108bd6766335b3b06bb847a084e76ca155dee
Aryan Kacker <kackeraryan@gmail.com>
2023-12-26 02:04:17 +0530

    Correct minor issues

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -106,13 +106,13 @@ const NewsReader = () => {
               onClick={() => addToFavorites(article)}
               className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline-blue"
             >
-              Add to Favorites
+              Add to Favourites
             </button>
           </div>
         ))}
       </div>
       <div className="mt-8">
-        <h1 className="text-5xl font-bold mb-4 text-white">Favorites</h1>
+        <h1 className="text-5xl font-bold mb-4 text-white">Favourites</h1>
         <button
           onClick={refreshPage}
           className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline-blue mb-4"

aa190e195d80639288c1dc80b40f88a537b3884c
Aryan Kacker <kackeraryan@gmail.com>
2023-12-26 01:40:10 +0530

    Added transitions and animations

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -2,7 +2,11 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-import { collection, getDocs, addDoc } from 'firebase/firestore';
+import { collection, getDocs, addDoc, deleteDoc, doc } from 'firebase/firestore';
+
+import { getAuth, signOut } from 'firebase/auth';
+
+import { useRouter } from 'next/navigation';
 
 import { db } from '../firebase/config';
 
@@ -10,6 +14,8 @@ const NewsReader = () => {
   const [news, setNews] = useState([]);
   const [favorites, setFavorites] = useState([]);
   const [description, setDescription] = useState([]);
+
+  const router = useRouter();
 
   useEffect(() => {
     const fetchNews = async () => {
@@ -52,16 +58,48 @@ const NewsReader = () => {
     }
   };
 
+  const removeFromFavorites = async (title) => {
+    try {
+      const favDocs = await getDocs(collection(db, 'favs'));
+      const docToDelete = favDocs.docs.find((doc)=> doc.data().title === title);
+      if(docToDelete){
+        await deleteDoc(doc(db, "title", docToDelete.id))
+      }
+      
+    } catch (error) {
+      console.error('Error removing document from favorites:', error);
+    }
+  };
+
   const refreshPage = () => {
     window.location.reload();
   };
 
+  const handleLogout = async () => {
+    const auth = getAuth();
+    try {
+      await signOut(auth);
+      console.log("Signout done")
+      router.push('/sign-in')
+    } catch (error) {
+      console.error('Error logging out:', error);
+    }
+  };
+
   return (
-    <div className="bg-gray-800 text-white min-h-screen p-8">
-      <h1 className="text-4xl font-bold mb-8 text-white">Welcome to "News Read"</h1>
+        <div className="bg-gray-800 text-white min-h-screen p-8">
+        <div className="flex justify-end">
+        <button onClick={handleLogout} className="text-blue-500 hover:text-blue-300 focus:outline-none transition duration-300 text-lg">
+        Logout
+        </button>
+      </div>
+      <h1 className="text-5xl font-bold mb-2 text-white animate-pulse">Welcome to "News Read" 📰📖</h1>
+      <p className="text-gray-400 mb-8 translate-x-5 text-xl">
+        Your source for the latest and trending news articles.
+      </p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {news.map((article) => (
-          <div key={article.title} className="bg-gray-700 p-6 rounded-lg">
+          <div key={article.title} className="bg-gray-700 p-6 rounded-lg transform transition-transform hover:scale-105">
             <h2 className="text-xl font-semibold mb-4 text-white">{article.title}</h2>
             <p className="text-gray-400 mb-4">{article.description}</p>
             <button
@@ -74,7 +112,7 @@ const NewsReader = () => {
         ))}
       </div>
       <div className="mt-8">
-        <h2 className="text-2xl font-bold mb-4">Favorites</h2>
+        <h1 className="text-5xl font-bold mb-4 text-white">Favorites</h1>
         <button
           onClick={refreshPage}
           className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline-blue mb-4"
@@ -82,8 +120,16 @@ const NewsReader = () => {
           Refresh
         </button>
         {favorites.map((title, index) => (
-          <div key={index} className="bg-gray-700 p-6 rounded-lg mb-4">
-            <h2 className="text-xl font-semibold mb-4 text-white">{title}</h2>
+          <div key={title} className="bg-gray-700 p-6 rounded-lg mb-4 transform transition-transform hover:scale-101 transition-color hover:bg-gray-600">
+            <div className="flex justify-between items-center mb-4">
+              <h2 className="text-xl font-semibold text-white">{title}</h2>
+              <button
+                onClick={() => removeFromFavorites(title)}
+                className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline-red"
+                >
+                Remove
+              </button>
+            </div>
             <p className="text-gray-400">{description[index]}</p>
           </div>
         ))}
